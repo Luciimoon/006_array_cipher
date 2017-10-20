24de632765aae490d500624c370c013d4425a796
compsci <compsci@compsci>
2017-10-20 10:45:17 -0600

    Working except for the z wrap

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -1,10 +1,24 @@
 package caesar;
 
+import java.util.Scanner;
+
 public class BeginHere {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner kbd = new Scanner(System.in);
+		
+		
+		caesarCipher myCipher = new caesarCipher();
+		
+		System.out.println("Please enter the plain text to encode:");
+		String plainText = kbd.nextLine();
+		
+		System.out.println("Please enter the key:");
+		int key = kbd.nextInt();
+		System.out.println("Your ket is: " + key);
+		
+		String codedText = myCipher.encode(plainText, key);
+		System.out.println("Your secret message is: " + codedText);
 	}
 
 }
---
+++ src/caesar/readme.txt
@@ -0,0 +1,12 @@
+Julius Caesar (Roman General) is credited with our first "cipher."
+
+This is a shift chipher, where each alpha is shifted by the same number.
+for example
+
+key = 1
+a --> b, b --> c etc
+
+to decipher, reverse the process
+
+b - 1 --> A, c - key --> b
+--------------
