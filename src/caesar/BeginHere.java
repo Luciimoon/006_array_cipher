8cf4ab874a4fd7be0969ebf5d2125fcf04dfe9ed
compsci <compsci@compsci>
2017-11-09 10:34:13 -0700

    Decodme working

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -16,115 +16,68 @@ public class BeginHere {
 
 		while (running == true) {
 
-			Object[] possibilities = {"Encode", "Decode", "Crack", "Exit"};
+			Object[] possibilities = { "Encode", "Decode", "Crack", "Exit" };
 			String showFirst = "Encode";
-			 s = (String)JOptionPane.showInputDialog(
-			                    null,
-			                    "Please make your selection.",
-			                    "Start Here",
-			                    JOptionPane.PLAIN_MESSAGE,
-			                    null,
-			                    possibilities,
-			                    showFirst);
+			s = (String) JOptionPane.showInputDialog(null, "Please make your selection.", "Start Here",
+					JOptionPane.PLAIN_MESSAGE, null, possibilities, showFirst);
 
 			caesarCipher myCipher = new caesarCipher();
-			
-			
-			if(s.equalsIgnoreCase("encode")) {
-				
- 		
-			 msg = "Please enter your plain text to encode";
-			String plainText = JOptionPane.showInputDialog(null, msg);
 
-			msg = "Please enter the key:";
-			key = Integer.parseInt(JOptionPane.showInputDialog(null, msg));
+			if (s.equalsIgnoreCase("encode")) {
 
-			//System.out.println("Please enter the key:");
-
-			String codedText = myCipher.encode(plainText, key);
-
-			msg = "Your secret message is: " + codedText;
-			JOptionPane.showMessageDialog(null, msg);
-			
-			}//end of 'encode' code
-			
-			//if(s.equalsIgnoreCase("exit")) 
-				//System.exit(0);
-		
-		
-		
-		if(s.equalsIgnoreCase("decode")) {
-		
- 		
-		 msg = "Please enter your plain text to decode";
-		String plainText = JOptionPane.showInputDialog(null, msg);
-
-		msg = "Please enter the key:";
-		key = Integer.parseInt(JOptionPane.showInputDialog(null, msg));
-
-		//System.out.println("Please enter the key:");
-
-		String codedText = myCipher.encode(plainText, -key);
-		//int intText = Integer.parseInt(codedText);
-		//if (intText < 0)
-		//	codedText = "40";
-		
-			
-			
-		
-
-		msg = "Your secret message is: " + codedText;
-		JOptionPane.showMessageDialog(null, msg);
-		
-		}
-		
-		
-		
-		
-//weird code
-		//	if (s == null) s = "Cancelled";// .equals doesn't work, null is not a String
-		//	JOptionPane.showMessageDialog(null, "You have selected: " + s);
-			/*
-			switch (s) { // switch on String allowed in Java 7 and higher
-			
-			case "Encode":
-				
-				
-			    msg = "Please enter your plain text to encode";
+				msg = "Please enter your plain text to encode";
 				String plainText = JOptionPane.showInputDialog(null, msg);
 
 				msg = "Please enter the key:";
 				key = Integer.parseInt(JOptionPane.showInputDialog(null, msg));
 
-				System.out.println("Please enter the key:");
+				// System.out.println("Please enter the key:");
+
+				String codedText = myCipher.encode(plainText, key);
+
+				msg = "Your secret message is: " + codedText;
+				JOptionPane.showMessageDialog(null, msg);
+
+			} // end of 'encode' code
+
+			// if(s.equalsIgnoreCase("exit"))
+			// System.exit(0);
+
+			if (s.equalsIgnoreCase("decode")) {
+
+				msg = "Please enter your plain text to decode";
+				String plainText = JOptionPane.showInputDialog(null, msg);
+
+				msg = "Please enter the key:";
+				key = Integer.parseInt(JOptionPane.showInputDialog(null, msg));
+
+				// System.out.println("Please enter the key:");
+
+				String codedText = myCipher.encode(plainText, -key);
+
+				msg = "Your secret message is: " + codedText;
+				JOptionPane.showMessageDialog(null, msg);
+
+			}
+			
+			
+			if (s.equalsIgnoreCase("Cracker")) {
+
+				msg = "Please enter your plain text to crack";
+				String plainText = JOptionPane.showInputDialog(null, msg);
+
+				for (key = 0; key < 38; key++) {
+					
+				}
+				
+				
 
 				String codedText = myCipher.encode(plainText, key);
 
 				msg = "Your secret message is: " + codedText;
 				JOptionPane.showMessageDialog(null, msg);
 				
-				
-				break;
-				//break means all done, leave the switch block - almost always use break
-				//if no break, execution continues for all remaining cases
-			case "Decode":
-				JOptionPane.showMessageDialog(null, 
-					"We are now in the Decoder switch block - add some code!");
-				break;
-			case "Cracke":
-				JOptionPane.showMessageDialog(null, 
-					"We are now in the Cracker switch block - add some code!");
-				break;
-			case "Exit":
-				JOptionPane.showMessageDialog(null, 
-					"We are now in the Exit switch block - so add some code already to get us out of here!");
-				break;
-			default:
-				JOptionPane.showMessageDialog(null, 
-					"We are now in the Unknown Case or Cancelled switch block - what do you want to do?");
 			}
-			*/
-			
 
 			msg = "Do you want to encode another message? (y/n)";
 			goAgain = JOptionPane.showInputDialog(null, msg);
@@ -150,4 +103,3 @@ public class BeginHere {
 		}
 	}
 }
-
--- src/caesar/caesarCipher.java
+++ src/caesar/caesarCipher.java
@@ -14,12 +14,18 @@ public class caesarCipher {
 		String secretText = "";
 		int index, keyAdded;
 
+		 
+		
+		
 		for (int i = 0; i < plainText.length(); i++) {
 			if (plainText.charAt(i) != ' ') {
 				index = alpha.indexOf(plainText.charAt(i));
 				keyAdded = (index + key) % alpha.length();
+				if (keyAdded < 0)
+					keyAdded = 38;
 				secretText = secretText + alpha.charAt(keyAdded);
 				
+					
 			} else {
 				secretText = secretText + " ";
 			}
